7dba955268963f7a95595262537d7bec5a5d468f
Lily <egzozu.be.bas@gmail.com>
2024-11-20 18:46:18 +0300

    update gradle plugin

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -515,7 +515,7 @@ class AndroidPlatform extends PlatformTarget
 			"android.permission.REQUEST_INSTALL_PACKAGES"
 		]);
 		context.ANDROID_GRADLE_VERSION = project.config.getString("android.gradle-version", "8.9");
-		context.ANDROID_GRADLE_PLUGIN = project.config.getString("android.gradle-plugin", "8.6.0");
+		context.ANDROID_GRADLE_PLUGIN = project.config.getString("android.gradle-plugin", "8.7.1");
 		context.ANDROID_USE_ANDROIDX = project.config.getString("android.useAndroidX", "true");
 		context.ANDROID_ENABLE_JETIFIER = project.config.getString("android.enableJetifier", "false");
 
